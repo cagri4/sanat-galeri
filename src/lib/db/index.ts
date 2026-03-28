f42df6cecb51b148cf0b36fa57d829e8aa5f4458
Cagr Ca <cagri4@yandex.com>
2026-03-28 09:54:11 +0100

    fix: add ssl require and connection params to postgres client

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,5 +2,9 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
 
-const client = postgres(process.env.DATABASE_URL!, { prepare: false })
+const client = postgres(process.env.DATABASE_URL!, {
+  prepare: false,
+  ssl: 'require',
+  connection: { application_name: 'sanat-galeri' },
+})
 export const db = drizzle(client, { schema })
